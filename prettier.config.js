7af28a2bd1fb509cdfa863e008aa42a5bbe76937
Jacobi Petrucciani <8117202+jpetrucciani@users.noreply.github.com>
2018-10-02 23:14:05 -0400

    more explicit prettier config
    😆

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   printWidth: 100,
-  parser: "flow"
+  parser: "flow",
+  arrowParens: "always",
+  trailingComma: "es5",
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true
 };
